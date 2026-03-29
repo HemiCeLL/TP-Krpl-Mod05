738554f9cf70cb43c3232c4ee96577728502def6
HemiCeLL <maxthejanitor360@gmail.com>
2026-03-30 04:12:20 +0700

    commit message

--- TP_MODUL5_103022430015/Program.cs
+++ TP_MODUL5_103022430015/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace ModulPraktikan
+{
+    public class HaloGeneric
+    {
+        public void SapaUser<T>(T input)
+        {
+            Console.WriteLine($"Halo user {input}");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HaloGeneric halo = new HaloGeneric();
+
+            string namaPraktikan = "Nama";
+
+            halo.SapaUser(namaPraktikan);
+        }
+    }
+}
